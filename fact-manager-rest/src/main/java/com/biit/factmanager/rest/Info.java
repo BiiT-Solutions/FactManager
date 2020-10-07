6074921516b3d9fd5077bb621346e14a056cb1e9
Óscar Ferrando <oscar.ferrando@maistering.com>
2020-10-07 16:23:04 +0200

    healthcheck

--- fact-manager-rest/src/main/java/com/biit/factmanager/rest/Info.java
+++ fact-manager-rest/src/main/java/com/biit/factmanager/rest/Info.java
@@ -13,12 +13,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
@@ -31,6 +26,13 @@ public class Info {
     public Info(Environment environment, ApplicationArguments applicationArguments) {
         this.environment = environment;
         this.applicationArguments = applicationArguments;
+    }
+
+    @ApiOperation(value = "Basic method to check if the server is online.")
+    @RequestMapping(value = "/healthCheck", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public void healthCheck(HttpServletRequest httpRequest) {
+
     }
 
     @ApiIgnore
