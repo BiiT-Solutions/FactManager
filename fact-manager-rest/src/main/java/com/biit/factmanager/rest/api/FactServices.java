346f917dd78ace5aa10535760a88474e84ebc9de
Óscar Ferrando <oscar.ferrando@maistering.com>
2020-10-07 16:01:06 +0200

    fixed names

--- fact-manager-rest/src/main/java/com/biit/factmanager/rest/api/FactServices.java
+++ fact-manager-rest/src/main/java/com/biit/factmanager/rest/api/FactServices.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,26 +25,26 @@ public class FactServices {
 		this.factProvider = factProvider;
 	}
 
-	/*@ApiOperation(value = "Get all facts", notes = "Parameters:")
+	@ApiOperation(value = "Get all facts", notes = "Parameters:")
 	@ResponseStatus(value = HttpStatus.OK)
-	@GetMapping(value = "")
+	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Collection<Fact> getAllFacts(HttpServletRequest httpRequest) {
 		return factProvider.getAll();
 	}
 
 	@ApiOperation(value = "Adds a new fact", notes = "Parameters:\n"
 			+ "fact (required): Fact object to be added")
-	@ResponseStatus(value = HttpStatus.OK)
-	@DeleteMapping(value = "")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	@PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Fact addFact(@ApiParam(value = "Notification Request", required = true) @RequestBody Fact fact,
 						HttpServletRequest httpRequest) {
 		return factProvider.add(fact);
-	}*/
+	}
 
 	@ApiOperation(value = "Deletes a fact", notes = "Parameters:\n"
 			+ "fact (required): Fact object to be removed.")
 	@ResponseStatus(value = HttpStatus.OK)
-	@PostMapping(value = "")
+	@DeleteMapping(value = "")
 	public void deleteFact(@ApiParam(value = "Notification Request", required = true) @RequestBody Fact fact,
 						HttpServletRequest httpRequest) {
 		factProvider.delete(fact);
