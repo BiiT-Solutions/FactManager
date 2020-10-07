62eb6f7d501d0fe580d9e6deaf1c218ad4e344f7
Óscar Ferrando <oscar.ferrando@maistering.com>
2020-10-07 11:43:56 +0200

    update sqema

--- fact-manager-rest/src/main/resources/schema.sql
+++ fact-manager-rest/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS facts;
 CREATE TABLE facts (
   `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-  `fact` text,
+  `fact` varchar(255) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL
 );
