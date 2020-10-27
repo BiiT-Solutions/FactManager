0256dc7dabebfb85241361588abf22674bc246f1
Óscar Ferrando <oscar.ferrando@maistering.com>
2020-10-27 10:20:35 +0100

    use bigint not long

--- fact-manager-rest/src/main/resources/schema.sql
+++ fact-manager-rest/src/main/resources/schema.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS facts;
 CREATE TABLE facts (
   `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
-  `patient_id` long,
-  `organization_id` long,
+  `patient_id` bigint,
+  `organization_id` bigint,
   `category` varchar(190),
   `question` varchar(190),
   `answer` varchar(190),
   `score` double,
   `xpath` varchar(190),
   `examination_name` varchar(190),
-  `examination_version` long,
-  `company_id` long,
+  `examination_version` bigint,
+  `company_id` bigint,
   `created_at` datetime DEFAULT NULL
 );
