88d8a6e32a63b2196b7bf681a80095c6332445da
Óscar Ferrando <oscar.ferrando@maistering.com>
2020-10-27 11:04:32 +0100

    test to check the filters

--- fact-manager-persistence/src/test/java/com/biit/factmanager/test/FactRepositoryTests.java
+++ fact-manager-persistence/src/test/java/com/biit/factmanager/test/FactRepositoryTests.java
@@ -9,10 +9,17 @@ import org.springframework.test.context.testng.AbstractTransactionalTestNGSpring
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Collection;
+
 @SpringBootTest
 @Test(groups = "factRepository")
-@Rollback(true)
+@Rollback(false)
 public class FactRepositoryTests extends AbstractTransactionalTestNGSpringContextTests {
+
+	private static final long FACT_PATIENT_ID = 1;
+	private static final long FACT_COMPANY_ID = 1;
+	private static final long FACT_ORGANIZATION_ID = 1;
+	private static final String FACT_EXAMINATION_NAME = "test";
 
 	@Autowired
 	private FactRepository factRepository;
@@ -26,11 +33,33 @@ public class FactRepositoryTests extends AbstractTransactionalTestNGSpringContex
 
 	@Test(dependsOnMethods = "getAllAtTheBeginning")
 	private void addFact() {
-		fact =  factRepository.save(new Fact());
+		Fact factTosave = new Fact();
+		factTosave.setPatientId(FACT_PATIENT_ID);
+		factTosave.setExaminationName(FACT_EXAMINATION_NAME);
+		factTosave.setCompanyId(FACT_COMPANY_ID);
+		factTosave.setOrganizationId(FACT_ORGANIZATION_ID);
+		fact =  factRepository.save(factTosave);
 		Assert.assertEquals(factRepository.count(), 1);
 	}
 
 	@Test(dependsOnMethods = "addFact")
+	private void getFilteredFacts() {
+		Assert.assertEquals(factRepository.count(), 1);
+		// Filter by patientId and ExaminationName
+		Collection<Fact> retrievedFacts =  factRepository.findByPatientIdAndExaminationName(FACT_PATIENT_ID,
+				FACT_EXAMINATION_NAME);
+		// Filter by companyId and ExaminationName
+		retrievedFacts =  factRepository.findByCompanyIdAndExaminationName(FACT_COMPANY_ID,
+				FACT_EXAMINATION_NAME);
+		Assert.assertEquals(retrievedFacts.size(), 1);
+
+		// Filter by organizationId and ExaminationName
+		retrievedFacts =  factRepository.findByOrganizationIdAndExaminationName(FACT_ORGANIZATION_ID,
+				FACT_EXAMINATION_NAME);
+		Assert.assertEquals(retrievedFacts.size(), 1);
+	}
+
+	@Test(dependsOnMethods = "getFilteredFacts")
 	private void deleteFact() {
 		factRepository.delete(fact);
 		Assert.assertEquals(factRepository.count(), 0);
