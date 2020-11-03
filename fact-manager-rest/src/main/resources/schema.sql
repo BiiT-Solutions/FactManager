aa14bf3d659a515f7f320c3481ac838db50d1b7e
Óscar Ferrando <oscar.ferrando@maistering.com>
2020-11-03 12:58:27 +0100

    do not drop table

--- fact-manager-rest/src/main/resources/schema.sql
+++ fact-manager-rest/src/main/resources/schema.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS facts;
-CREATE TABLE facts (
+CREATE TABLE IF NOT EXISTS facts (
   `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
   `patient_id` bigint,
   `organization_id` bigint,
