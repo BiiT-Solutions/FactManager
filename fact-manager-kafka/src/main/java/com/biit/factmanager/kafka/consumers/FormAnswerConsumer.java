f17728ec33e3529411c505f733c25b640e085898
Oscar Ferrando <oscar.ferrando@maistering.com>
2021-04-30 23:13:59 +0200

    do not use hardoced properties

--- fact-manager-kafka/src/main/java/com/biit/factmanager/kafka/consumers/FormAnswerConsumer.java
+++ fact-manager-kafka/src/main/java/com/biit/factmanager/kafka/consumers/FormAnswerConsumer.java
@@ -2,6 +2,7 @@ package com.biit.factmanager.kafka.consumers;
 
 import com.biit.factmanager.core.providers.FactProvider;
 import com.biit.factmanager.logger.FactManagerLogger;
+import com.biit.factmanager.persistence.configuration.FactManagerConfigurationReader;
 import com.biit.factmanager.persistence.entities.Fact;
 import com.biit.kafkaclient.KafkaConsumerClient;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -9,7 +10,11 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
 import java.util.function.Consumer;
 
 @Service
@@ -19,13 +24,14 @@ public class FormAnswerConsumer extends KafkaConsumerClient {
     private FactProvider factProvider;
 
     private static final String TOPIC_NAME = "FormAnswer";
+    // private static final List<Fact> CLASS_TYPE = new ArrayList<>();
 
     public FormAnswerConsumer(){
         super(Fact.class);
         new Thread(() -> { // Start async and capture errors in the case there is no kafka running
             try {
-                // TODO (oferrando): remove hardcoded properties
-                setProperties(getPropertiesHardcoded());
+                // Uncomment if you want to override standard kafka properties
+                // setProperties(getPropertiesHardcoded());
                 FactManagerLogger.debug(this.getClass().getName(), "Starting FormAnswersConsumer");
                 startConsumer(Collections.singleton(TOPIC_NAME), getConsumer());
                 FactManagerLogger.debug(this.getClass().getName(), "Started FormAnswersConsumer");
@@ -38,7 +44,7 @@ public class FormAnswerConsumer extends KafkaConsumerClient {
 
     private Consumer getConsumer(){
         return (x) -> {
-            FactManagerLogger.debug(this.getClass().getName(), "fact to save " + x.toString());
+            FactManagerLogger.debug(this.getClass().getName(), "FormAnswer event to save " + x.toString());
             final Fact savedFact = factProvider.add((Fact) x);
             FactManagerLogger.debug(this.getClass().getName(), "Saved fact " + savedFact.toString());
         };
@@ -47,15 +53,45 @@ public class FormAnswerConsumer extends KafkaConsumerClient {
     public Properties getPropertiesHardcoded() {
         // TODO (oferrando): remove
         final Properties result = new Properties();
-        // result.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, "ID" + Math.abs(new Random().nextLong()));
-        result.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, "ID" + 28L);
-        result.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        result.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        result.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
-        result.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        result.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        result.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, "ID" + Math.abs(new SecureRandom().nextInt(Integer.MAX_VALUE)));
+        //result.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, "ID" + 28L);
+        result.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, FactManagerConfigurationReader.getInstance().getKeyDeserializerClassConfig());
+        result.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, FactManagerConfigurationReader.getInstance().getValueDeserializerClassConfig());
+        result.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, FactManagerConfigurationReader.getInstance().getBootstrapServersConfig());
+        result.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, FactManagerConfigurationReader.getInstance().getKeySerializerClassConfig());
+        result.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, FactManagerConfigurationReader.getInstance().getValueSerializerClassConfig());
         return result;
     }
 
+
+    private Consumer getConsumer2(){
+        return (x) -> {
+            FactManagerLogger.debug(this.getClass().getName(), "Event consumed1 " + x.toString());
+            final List<Fact> facts = (ArrayList<Fact>) x;
+            FactManagerLogger.debug(this.getClass().getName(), "Event consumed2 " + facts.toString());
+            FactManagerLogger.debug(this.getClass().getName(), "Event consumed3 " + x.toString());
+            /*while (facts.iterator().hasNext()){
+                FactManagerLogger.debug(this.getClass().getName(), "fact sto save " + facts.iterator().next().toString());
+            }*/
+            /*for (final Object fact: facts){
+                FactManagerLogger.debug(this.getClass().getName(), "fact to save " + fact.toString());
+
+                final Fact castedFact;
+                try {
+                    castedFact = JacksonSerializer.getDefaultSerializer().readValue(fact.toString(), Fact.class);
+                    FactManagerLogger.debug(this.getClass().getName(), "fact to save " + (castedFact instanceof  Fact));
+                    FactManagerLogger.debug(this.getClass().getName(), "fact to save " + castedFact.toString());
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }*/
+            facts.forEach(fact -> {
+                FactManagerLogger.debug(this.getClass().getName(), "fact to save " + fact.toString());
+                final Fact savedFact = factProvider.add((Fact) fact);
+                FactManagerLogger.debug(this.getClass().getName(), "Saved fact " + savedFact.toString());
+            });
+
+        };
+    }
 
 }
