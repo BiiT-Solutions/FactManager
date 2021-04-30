0ab8fc0737335a1f29790279d342db6a000709d2
Oscar Ferrando <oscar.ferrando@maistering.com>
2021-04-30 23:30:34 +0200

    preapre to add tests

---
+++ fact-manager-kafka/src/test/java/com/biit/factmanager/FactManagerServer.java
@@ -0,0 +1,23 @@
+package com.biit.factmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+@ComponentScan({"com.biit.factmanager"})
+@EnableJpaRepositories({"com.biit.factmanager.persistence.repositories"})
+@EntityScan({"com.biit.factmanager.persistence.entities"})
+@Service
+
+public class FactManagerServer {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactManagerServer.class, args);
+	}
+
+}
