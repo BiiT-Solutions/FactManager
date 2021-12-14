393c3078f3f7b5a0b077e13b961799b1e7d9100a
Jorge Hortelano <jhortelano@biit-solutions.com>
2021-12-14 18:27:26 +0100

    Adding StringFact search values tests

--- fact-manager-persistence/src/test/java/com/biit/factmanager/test/StringFactRepositoryTests.java
+++ fact-manager-persistence/src/test/java/com/biit/factmanager/test/StringFactRepositoryTests.java
@@ -1,9 +1,11 @@
 package com.biit.factmanager.test;
 
+import com.biit.factmanager.persistence.entities.FormRunnerFact;
 import com.biit.factmanager.persistence.entities.StringFact;
 import com.biit.factmanager.persistence.repositories.FactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.util.Pair;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.testng.Assert;
@@ -11,6 +13,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @SpringBootTest
@@ -18,6 +21,7 @@ import java.util.List;
 @Rollback(false)
 public class StringFactRepositoryTests extends AbstractTransactionalTestNGSpringContextTests {
     private static final String STRING_FACT = "stringFact";
+    private static final String NEW_STRING_FACT = "newStringFact";
     private static final String STRING_FACT_UPDATED = "stringFactUpdated";
     private static final String FACT_ELEMENT_ID = "factElementId";
     private static final LocalDateTime FACT_TIME_FUTURE = LocalDateTime.now().plusDays(1);
@@ -42,27 +46,53 @@ public class StringFactRepositoryTests extends AbstractTransactionalTestNGSpring
         stringFactRepository.save(stringFact);
         Assert.assertTrue(stringFactRepository.findByElementIdAndCreatedAtGreaterThanEqualAndCreatedAtLessThanEqual
                 (FACT_ELEMENT_ID, FACT_TIME_PAST, FACT_TIME_FUTURE).contains(stringFact));
+        stringFactRepository.delete(stringFact);
     }
 
     @Test(dependsOnMethods = "createStringFact")
+    private void searchFactByValue() {
+        Collection<StringFact> facts = stringFactRepository.findByValueParameters(Pair.of("string", STRING_FACT));
+        Assert.assertEquals(facts.size(), 1);
+    }
+
+    @Test(dependsOnMethods = "createStringFact")
+    private void searchFactByInvalidValue() {
+        Collection<StringFact> facts = stringFactRepository.findByValueParameters(Pair.of("string", STRING_FACT + "!"));
+        Assert.assertEquals(facts.size(), 0);
+    }
+
+    @Test(dependsOnMethods = {"searchFactByValue", "searchFactByInvalidValue"})
     private void updateStringFact() throws Exception {
         StringFact stringFact = new StringFact();
-        stringFact.setString(STRING_FACT);
+        stringFact.setString(NEW_STRING_FACT);
         StringFact savedStringFact = stringFactRepository.save(stringFact);
-        Assert.assertEquals((stringFactRepository.findById(savedStringFact.getId()).orElseThrow(Exception::new)).getString(), STRING_FACT);
+        Assert.assertEquals((stringFactRepository.findById(savedStringFact.getId()).orElseThrow(Exception::new)).getString(), NEW_STRING_FACT);
         StringFact updatedStringFact = stringFactRepository.findById(savedStringFact.getId()).orElseThrow(Exception::new);
         updatedStringFact.setString(STRING_FACT_UPDATED);
         stringFactRepository.save(updatedStringFact);
+        Assert.assertEquals(stringFactRepository.findAll().size(), 2);
         Assert.assertEquals((stringFactRepository.findById(savedStringFact.getId()).orElseThrow(Exception::new)).getString(), STRING_FACT_UPDATED);
     }
 
-    @Test(dependsOnMethods = {"createStringFact", "updateStringFact", "readFact"})
+    @Test(dependsOnMethods = "updateStringFact")
+    private void searchFactByUpdatedValue() {
+        Collection<StringFact> facts = stringFactRepository.findByValueParameters(Pair.of("string", STRING_FACT_UPDATED));
+        Assert.assertEquals(facts.size(), 1);
+    }
+
+    @Test(dependsOnMethods = "updateStringFact")
+    private void searchFactByOldValue() {
+        Collection<StringFact> facts = stringFactRepository.findByValueParameters(Pair.of("string", NEW_STRING_FACT));
+        Assert.assertEquals(facts.size(), 0);
+    }
+
+    @Test(dependsOnMethods = {"updateStringFact", "searchFactByUpdatedValue", "searchFactByOldValue"})
     private void deleteStringFact() {
-        Assert.assertEquals(stringFactRepository.count(), 3);
-        List<StringFact> stringFacts = stringFactRepository.findAll();
-        Assert.assertEquals(stringFacts.size(), 3);
-        stringFactRepository.delete(stringFacts.get(0));
         Assert.assertEquals(stringFactRepository.count(), 2);
+        List<StringFact> stringFacts = stringFactRepository.findAll();
+        Assert.assertEquals(stringFacts.size(), 2);
+        stringFactRepository.delete(stringFacts.get(0));
+        Assert.assertEquals(stringFactRepository.count(), 1);
     }
 
     @AfterClass
