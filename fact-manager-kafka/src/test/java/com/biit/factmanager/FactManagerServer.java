7284f37db1de8fc5429baa8d38f091879c5aee95
Jorge Hortelano <jorge.hortelano@maistering.com>
2022-01-18 20:09:18 +0100

    Correcting properties

--- fact-manager-kafka/src/test/java/com/biit/factmanager/FactManagerServer.java
+++ fact-manager-kafka/src/test/java/com/biit/factmanager/FactManagerServer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +12,8 @@ import org.springframework.stereotype.Service;
 @ComponentScan({"com.biit.factmanager"})
 @EnableJpaRepositories({"com.biit.factmanager.persistence.repositories"})
 @EntityScan({"com.biit.factmanager.persistence.entities"})
+@PropertySource("classpath:application.properties")
 @Service
-
 public class FactManagerServer {
 
 
--- fact-manager-kafka/src/test/java/com/biit/factmanager/test/KafkaTests.java
+++ fact-manager-kafka/src/test/java/com/biit/factmanager/test/KafkaTests.java
@@ -1,10 +1,15 @@
 package com.biit.factmanager.test;
 
+import com.biit.factmanager.core.providers.FactProvider;
+import com.biit.factmanager.kafka.consumers.FormAnswerConsumer;
 import com.biit.factmanager.persistence.entities.FormRunnerFact;
 import com.biit.factmanager.persistence.entities.values.FormRunnerValue;
 import com.biit.kafkaclient.IKafkaConsumerClient;
 import com.biit.kafkaclient.KafkaConsumerClient;
 import com.biit.kafkaclient.KafkaProducerClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -16,12 +21,16 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
+@SpringBootTest
 @Test(groups = {"kafkaEvents"})
-public class KafkaTests {
+public class KafkaTests extends AbstractTransactionalTestNGSpringContextTests {
     private static final String TOPIC_NAME = "facts";
     private static final int EVENTS_QUANTITY = 100;
     private static final String QUESTION = "Question? ";
     private static final String ANSWER = "Answer: ";
+
+    @Autowired
+    private FactProvider<FormRunnerFact> factProvider;
 
     private FormRunnerFact generateEvent(int value) {
         FormRunnerFact formRunnerFact = new FormRunnerFact();
@@ -46,7 +55,7 @@ public class KafkaTests {
     }
 
     public synchronized void factTest() throws InterruptedException {
-        IKafkaConsumerClient<FormRunnerFact> kafkaClient = new KafkaConsumerClient<>(FormRunnerFact.class);
+        FormAnswerConsumer kafkaClient = new FormAnswerConsumer(factProvider);
         Set<FormRunnerFact> consumerEvents = Collections.synchronizedSet(new HashSet<>(EVENTS_QUANTITY));
         Set<FormRunnerFact> producerEvents = new HashSet<>(EVENTS_QUANTITY);
         //Store received events into set.
@@ -61,7 +70,7 @@ public class KafkaTests {
     }
 
     public synchronized void multipleProducerTest() throws InterruptedException {
-        IKafkaConsumerClient<FormRunnerFact> kafkaClient = new KafkaConsumerClient<>(FormRunnerFact.class);
+        IKafkaConsumerClient<FormRunnerFact> kafkaClient = new FormAnswerConsumer(factProvider);
         Set<FormRunnerFact> consumerEvents = Collections.synchronizedSet(new HashSet<>(EVENTS_QUANTITY * 2));
         Set<FormRunnerFact> producerEvents = new HashSet<>(EVENTS_QUANTITY);
         Set<FormRunnerFact> producerEvents2 = new HashSet<>(EVENTS_QUANTITY);
@@ -80,7 +89,7 @@ public class KafkaTests {
     }
 
     public synchronized void multipleConsumerTest() throws InterruptedException {
-        IKafkaConsumerClient<FormRunnerFact> kafkaClient = new KafkaConsumerClient<>(FormRunnerFact.class);
+        IKafkaConsumerClient<FormRunnerFact> kafkaClient = new FormAnswerConsumer(factProvider);
         Set<FormRunnerFact> consumerEvents = Collections.synchronizedSet(new HashSet<>(EVENTS_QUANTITY));
         IKafkaConsumerClient<FormRunnerFact> kafkaClient2 = new KafkaConsumerClient<>(FormRunnerFact.class);
         Set<FormRunnerFact> consumerEvents2 = Collections.synchronizedSet(new HashSet<>(EVENTS_QUANTITY));
@@ -102,7 +111,7 @@ public class KafkaTests {
         LocalDateTime initialDate = LocalDateTime.of(2016, Calendar.FEBRUARY, 1, 0, 0, 0);
         LocalDateTime finalDate = LocalDateTime.of(2016, Calendar.MAY, 1, 23, 59, 59);
 
-        IKafkaConsumerClient<FormRunnerFact> kafkaClient = new KafkaConsumerClient<>(FormRunnerFact.class);
+        IKafkaConsumerClient<FormRunnerFact> kafkaClient = new FormAnswerConsumer(factProvider);
         Set<FormRunnerFact> consumerEvents = Collections.synchronizedSet(new HashSet<>(EVENTS_QUANTITY));
         Set<FormRunnerFact> producerEvents = new HashSet<>(EVENTS_QUANTITY);
         kafkaClient.startConsumer(Collections.singleton(TOPIC_NAME), basicEvent -> {
