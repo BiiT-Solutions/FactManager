544fb41b643c069b28cee649736acd3e3827f867
SimonBC3 <sbrotons3@gmail.com>
2022-02-17 11:52:33 +0100

    changes to html composition.

--- fact-manager-core/src/test/java/com/biit/factmanager/test/ChartProviderTest.java
+++ fact-manager-core/src/test/java/com/biit/factmanager/test/ChartProviderTest.java
@@ -27,12 +27,12 @@ public class ChartProviderTest {
 
     @BeforeClass
     public void populate() {
-        for(int j = 0; j <= 2; j++) {
-            for(int i = 0; i < 5; i++) {
+        for (int j = 0; j <= 2; j++) {
+            for (int i = 0; i < 5; i++) {
                 FormRunnerFact formRunnerFact = new FormRunnerFact();
                 FormRunnerValue formRunnerValue = new FormRunnerValue();
 
-                formRunnerValue.setScore((double) (i+j));
+                formRunnerValue.setScore((double) (i + j));
                 formRunnerFact.setTenantId("persona" + j);
                 formRunnerFact.setEntity(formRunnerValue);
 
@@ -48,20 +48,20 @@ public class ChartProviderTest {
         final StringBuilder html = new StringBuilder();
         html.append("<!DOCTYPE html>\n")
                 .append("<html lang=\"en\">\n")
-                .append("    <head>\n")
-                .append("        <meta charset=\"utf-8\">\n")
-                .append("        <title>C3</title>\n")
-                .append("    </head>\n\n")
-                .append("    <body>\n")
+                .append("<head>\n")
+                .append("<meta charset=\"utf-8\">\n")
+                .append("<title>C3</title>\n")
+                .append("</head>\n\n")
+                .append("<body>\n")
                 .append("<h1>C3 example</h1>\n")
-                .append("        <div id=\"chart\"></div>\n\n")
-                .append("        <script src=\"https://d3js.org/d3.v5.min.js\"></script>\n")
-                .append("        <script src=\"c3.min.js\"></script>\n");
+                .append("<div id=\"chart\"></div>\n\n")
+                .append("<script src=\"https://d3js.org/d3.v5.min.js\"></script>\n")
+                .append("<script src=\"c3.min.js\"></script>\n");
         html.append("<script>");
         html.append("var chart = c3.generate({\n")
-                .append("            bindto: '#chart',\n")
-                .append("            data: {\n")
-                .append("              columns: [\n");
+                .append("bindto: '#chart',\n")
+                .append("data: {\n")
+                .append("columns: [\n");
 
         getUniqueTenants().forEach(tenant -> {
             html.append("[ '" + tenant + "'");
@@ -74,24 +74,24 @@ public class ChartProviderTest {
         });
 
         html.append("],\n")
-                .append("              axes: {\n")
-                .append("                data2: 'y2'\n")
-                .append("              },\n")
-                .append("              type: '"+ type + "'\n},\n")
-                .append("   axis: {\n")
-                .append("              y2: {\n")
-                .append("                show: true,\n")
-                .append("                label: {\n")
-                .append("                    text: 'Y2',\n")
-                .append("                    position: 'outer-middle' //outer inner top bottom\n},\n")
-                .append("                tick: {\n")
-                .append("                    format: d3.format(\"$\")\n}\n}\n}\n});\n</script>\n</body>\n</html>");
+                .append("axes: {\n")
+                .append("data2: 'y2'\n")
+                .append("},\n")
+                .append("type: '" + type + "'\n},\n")
+                .append("axis: {\n")
+                .append("y2: {\n")
+                .append("show: true,\n")
+                .append("label: {\n")
+                .append("text: 'Y2',\n")
+                .append("position: 'outer-middle' //outer inner top bottom\n},\n")
+                .append("tick: {\n")
+                .append("format: d3.format(\"$\")\n}\n}\n}\n});\n</script>\n</body>\n</html>");
 
         System.out.println(html);
     }
 
 
-    private  List<String> getUniqueTenants() {
+    private List<String> getUniqueTenants() {
         List<String> tenants = new ArrayList<>();
         formRunnerFacts.forEach(formRunnerFact -> tenants.add(formRunnerFact.getTenantId()));
         return tenants.stream().distinct().collect(Collectors.toList());
