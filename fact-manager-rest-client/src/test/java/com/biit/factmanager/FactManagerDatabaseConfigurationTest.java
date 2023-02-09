a6ee419bfd020dd29c52351da6dbf98caa32e266
Jorge Hortelano <jhortelano@biit-solutions.com>
2023-02-09 13:45:20 +0100

    Filtering facts by class

---
+++ fact-manager-rest-client/src/test/java/com/biit/factmanager/FactManagerDatabaseConfigurationTest.java
@@ -0,0 +1,54 @@
+package com.biit.factmanager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.HashMap;
+
+@Configuration
+@EnableJpaRepositories(entityManagerFactoryRef = "factmanagerSystemFactoryApp", transactionManagerRef = "factmanagerTransactionManagerApp", basePackages = {
+        FactManagerDatabaseConfigurationTest.PACKAGE})
+public class FactManagerDatabaseConfigurationTest {
+    public static final String PACKAGE = "com.biit.factmanager.persistence";
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.factmanager.datasource")
+    public DataSource factmanagerDataSourceApp() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "factmanagerSystemFactoryApp")
+    @Primary
+    public LocalContainerEntityManagerFactoryBean factmanagerSystemFactoryApp(EntityManagerFactoryBuilder builder,
+                                                                           @Qualifier("factmanagerDataSourceApp") DataSource dataSource) {
+        final HashMap<String, Object> properties = new HashMap<>();
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("spring.factmanager.datasource.jpa.hibernate.ddl-auto"));
+        properties.put("hibernate.dialect", environment.getProperty("spring.factmanager.datasource.hibernate.dialect"));
+        return builder.dataSource(dataSource).properties(properties).packages(PACKAGE).build();
+    }
+
+    @Bean
+    @Primary
+    public PlatformTransactionManager factmanagerTransactionManagerApp(
+            @Qualifier("factmanagerSystemFactoryApp") EntityManagerFactory factmanagerSystemFactoryApp) {
+        return new JpaTransactionManager(factmanagerSystemFactoryApp);
+    }
+
+}
