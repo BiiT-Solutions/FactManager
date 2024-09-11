d8414f06736e940ddf43f298c0a77ea9b597800f
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-09-11 11:52:42 +0200

    Update exceptions to include frontend codes

--- fact-manager-rest/src/main/java/com/biit/factmanager/rest/ExceptionControllerAdvice.java
+++ fact-manager-rest/src/main/java/com/biit/factmanager/rest/ExceptionControllerAdvice.java
@@ -1,86 +1,26 @@
 package com.biit.factmanager.rest;
 
 import com.biit.factmanager.core.providers.exceptions.FactNotFoundException;
-import com.biit.factmanager.core.providers.exceptions.InvalidParameterException;
 import com.biit.factmanager.logger.FactManagerLogger;
-import com.biit.server.exceptions.BadRequestException;
+import com.biit.server.exceptions.ErrorResponse;
 import com.biit.server.exceptions.ServerExceptionControllerAdvice;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 
 //@ControllerAdvice
 public class ExceptionControllerAdvice extends ServerExceptionControllerAdvice {
 
-    @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(
-            HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-        FactManagerLogger.errorMessage(this.getClass(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<Object> handleNullPointer(NullPointerException ex) {
-        FactManagerLogger.errorMessage(this.getClass(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
-        FactManagerLogger.errorMessage(this.getClass(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(FactNotFoundException.class)
     public ResponseEntity<Object> factNotFoundException(IllegalArgumentException ex) {
         FactManagerLogger.errorMessage(this.getClass(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(InvalidParameterException.class)
-    public ResponseEntity<Object> invalidParameterException(IllegalArgumentException ex) {
-        FactManagerLogger.errorMessage(this.getClass(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> unknownException(Exception ex) {
-        FactManagerLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<Object> badRequestException(Exception ex) {
-        FactManagerLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "fact_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Object> runtimeException(Exception ex) {
         FactManagerLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
-            HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status,
-            WebRequest request) {
-        FactManagerLogger.errorMessage(this.getClass(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
-    }
-
-    @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
-            HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatusCode status,
-            WebRequest request) {
-        FactManagerLogger.errorMessage(this.getClass(), ex);
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), ex), HttpStatus.BAD_REQUEST);
     }
 }
