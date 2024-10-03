429e7637ae7fa867d549ee46e5e63c1c2af63610
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-10-03 14:56:00 +0200

    Changed method to patch

--- fact-manager-rest/src/main/java/com/biit/factmanager/rest/api/FactServices.java
+++ fact-manager-rest/src/main/java/com/biit/factmanager/rest/api/FactServices.java
@@ -17,9 +17,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -343,7 +343,7 @@ public class FactServices<V> {
             security = @SecurityRequirement(name = "bearerAuth"))
     @PreAuthorize("hasAnyAuthority(@securityService.adminPrivilege)")
     @ResponseStatus(value = HttpStatus.OK)
-    @PutMapping(value = "/update/session/{session}")
+    @PatchMapping(value = "/update/session/{session}")
     public void updateSessionFacts(
             @Parameter(description = "UUID from a session", required = true) @PathVariable("session") String session,
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
