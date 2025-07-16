335e6f2e7891987aa3c27ffd3dc259f248882d07
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-16 10:34:01 +0200

    Adding organization clientAdding organization admin privilege

--- fact-manager-rest/src/main/java/com/biit/factmanager/rest/FactManagerSecurityService.java
+++ fact-manager-rest/src/main/java/com/biit/factmanager/rest/FactManagerSecurityService.java
@@ -11,11 +11,14 @@ public class FactManagerSecurityService extends SecurityService {
     private static final String VIEWER = "FactManager_VIEWER";
     private static final String ADMIN = "FactManager_ADMIN";
     private static final String EDITOR = "FactManager_EDITOR";
+    private static final String ORGANIZATION_ADMIN = "FactManager_ORGANIZATION_ADMIN";
 
     private String viewerPrivilege = null;
     private String adminPrivilege = null;
     private String editorPrivilege = null;
+    private String organizationAdminPrivilege = null;
 
+    @Override
     public String getViewerPrivilege() {
         if (viewerPrivilege == null) {
             viewerPrivilege = VIEWER.toUpperCase();
@@ -23,6 +26,7 @@ public class FactManagerSecurityService extends SecurityService {
         return viewerPrivilege;
     }
 
+    @Override
     public String getAdminPrivilege() {
         if (adminPrivilege == null) {
             adminPrivilege = ADMIN.toUpperCase();
@@ -30,10 +34,19 @@ public class FactManagerSecurityService extends SecurityService {
         return adminPrivilege;
     }
 
+    @Override
     public String getEditorPrivilege() {
         if (editorPrivilege == null) {
             editorPrivilege = EDITOR.toUpperCase();
         }
         return editorPrivilege;
+    }
+
+    @Override
+    public String getOrganizationAdminPrivilege() {
+        if (organizationAdminPrivilege == null) {
+            organizationAdminPrivilege = ORGANIZATION_ADMIN.toUpperCase();
+        }
+        return organizationAdminPrivilege;
     }
 }
